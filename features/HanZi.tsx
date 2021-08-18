958ded273c5f04ea5413c29a1ba913e6414c5b1d
DumbledoreD <dumbledoredeng@gmail.com>
2021-08-18 00:56:50 +0000

    Add han zi

---
+++ features/HanZi.tsx
@@ -0,0 +1,121 @@
+import React, { useRef, useEffect } from "react";
+import * as d3 from "d3";
+
+const HanZi = ({
+  children,
+  containerClass,
+}: {
+  children: string;
+  containerClass: string;
+}) => {
+  const svgRef = useRef(null);
+
+  const width = 210,
+    height = 60,
+    color = "white",
+    stroke = "#c41b14",
+    strokeWidth = 3,
+    left = 55,
+    top = 5,
+    size = 50,
+    offset = 7;
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+
+    //内横线
+    svg
+      .selectAll("line.inner-horizontal")
+      .data(children)
+      .enter()
+      .append("line")
+      .classed("inner-horizontal", true)
+      .attr("x1", (d, i) => left + (i - 1) * size)
+      .attr("y1", top + size / 2)
+      .attr("x2", (d, i) => left + size + (i - 1) * size)
+      .attr("y2", top + size / 2)
+      .attr("stroke", stroke)
+      .attr("stroke-width", 1);
+
+    //内竖线
+    svg
+      .selectAll("line.inner-vertical")
+      .data(children)
+      .enter()
+      .append("line")
+      .classed("inner-vertical", true)
+      .attr("x1", (d, i) => left + size / 2 + (i - 1) * size)
+      .attr("y1", top)
+      .attr("x2", (d, i) => left + size / 2 + (i - 1) * size)
+      .attr("y2", top + size)
+      .attr("stroke", stroke)
+      .attr("stroke-width", 1);
+
+    //内斜线1
+    svg
+      .selectAll("line.inner-dashed1")
+      .data(children)
+      .enter()
+      .append("line")
+      .classed("inner-dashed1", true)
+      .attr("x1", (d, i) => left + (i - 1) * size)
+      .attr("y1", top)
+      .attr("x2", (d, i) => left + size + (i - 1) * size)
+      .attr("y2", top + size)
+      .attr("stroke", stroke)
+      .attr("stroke-dasharray", "5,5")
+      .attr("stroke-width", 1);
+
+    //内斜线2
+    svg
+      .selectAll("line.inner-dashed2")
+      .data(children)
+      .enter()
+      .append("line")
+      .classed("inner-dashed2", true)
+      .attr("x1", (d, i) => left + (i - 1) * size)
+      .attr("y1", top + size)
+      .attr("x2", (d, i) => left + size + (i - 1) * size)
+      .attr("y2", top)
+      .attr("stroke", stroke)
+      .attr("stroke-dasharray", "5,5")
+      .attr("stroke-width", 1);
+
+    //外框线
+    svg
+      .selectAll("rect.frame")
+      .data(children)
+      .enter()
+      .append("rect")
+      .classed("frame", true)
+      .attr("fill", "transparent")
+      .attr("stroke", stroke)
+      .attr("stroke-width", strokeWidth)
+      .attr("x", (d, i) => left + (i - 1) * size)
+      .attr("y", top)
+      .attr("width", size)
+      .attr("height", size);
+
+    //汉字
+    svg
+      .selectAll("text.chars")
+      .data(children)
+      .enter()
+      .append("text")
+      .classed("chars", true)
+      .attr("x", (d, i) => left + (i - 1) * size)
+      .attr("y", top + size - offset)
+      .style("font-size", `${size}px`)
+      .style("font-family", "Kaiti")
+      .attr("fill", color)
+      .text((d) => d);
+  }, [children]);
+
+  return (
+    <div className={containerClass}>
+      <svg ref={svgRef} width={width} height={height} />
+    </div>
+  );
+};
+
+export default HanZi;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import Head from "next/head";
 
 import styles from "styles/home.module.css";
 
+import HanZi from "features/HanZi";
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>D for Dilly Dally</title>
         <meta name="description" content="I'm busy dilly dallying." />
+        {/* Ref: https://css-tricks.com/emojis-as-favicons/ */}
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😎</text></svg>"
@@ -20,14 +23,15 @@ const Home: NextPage = () => {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
         />
       </Head>
 
       <main
         className={`${styles.background} ${styles.fullScreen} ${styles.flexBox}`}
       >
+        <HanZi containerClass={styles.hanZi}>游手好闲</HanZi>
         <h1 className={`${styles.header} ${styles.glow}`}>D4DillyDally</h1>
       </main>
     </>
