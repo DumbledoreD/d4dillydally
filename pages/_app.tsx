be149f4b00d5524f984f0cd0468b7b5f9e48907a
DumbledoreD <dumbledoredeng@gmail.com>
2021-08-19 00:26:32 +0000

    Set up MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+// Ref: https://github.com/mui-org/material-ui/tree/next/examples/nextjs-with-typescript
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createEmotionCache, theme } from "features/theme";
+import { AppProps } from "next/app";
+import Head from "next/head";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp = (props: MyAppProps): JSX.Element => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 };
 
 export default MyApp;
