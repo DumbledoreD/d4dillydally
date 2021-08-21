05c37bebf693ef1cad9add5bd90f028ba9e0026d
DumbledoreD <dumbledoredeng@gmail.com>
2021-08-21 15:09:59 +0000

    Add calendar page

---
+++ pages/calendar.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Calendar</title>
+      </Head>
+
+      <main>Calendar</main>
+    </>
+  );
+};
+
+export default Home;
